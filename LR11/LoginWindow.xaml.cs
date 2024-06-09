1de743eb4b87622aae9027db2d02533540622c14
Medvedezz <zborovsky25@gmail.com>
2024-06-09 21:55:12 +0300

    Добавьте файлы проекта.

---
+++ LR11/LoginWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace LR11
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (LoginTextBox.Text == "admin" && PasswordBox.Password == "admin")
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close(); 
+            else
+            {
+                MessageBox.Show("Неправильний логін або пароль!", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ LR11/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LR11
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                string message = $"Натиснули кнопку {button.Content}";
+                MessageBox.Show(message, "Інформація", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+                if (button.Content.ToString() == "Вийти")
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+    }
+}
